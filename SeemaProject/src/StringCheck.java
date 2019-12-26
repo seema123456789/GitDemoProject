48e7cb7280a681845f9cc63657a4d70d9fa93b12
SEEMA TRIPATHI <seema.tripathi1212@gmail.com>
2019-12-26 19:56:19 +0530

    first commit

---
+++ SeemaProject/src/StringCheck.java
@@ -0,0 +1,18 @@
+
+public class StringCheck {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String s= null;
+		String s2= "Seema";
+		//Boolean s1= s.isEmpty();
+		//System.out.println(s1);//npe
+		System.out.println(s2.equals(s));//false
+		System.out.println(s.equals(s2));//npe
+
+
+
+	}
+
+}
